392529d3a67e64c561861eb09b46e81f27b33596
tqduy02 <98021569+tqduy02@users.noreply.github.com>
2023-12-16 07:44:51 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,16 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int number;
+		String nameSchool;
+
+	}
+
+	public void show() {
+
+	}
+
+}
